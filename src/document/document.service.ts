4e864e85855bd85dc24573e4d9c6178074afc9a3
John Baker <john.baker@aspireiq.com>
2025-03-02 12:41:04 -0500

    feat(document): document service logic

--- src/document/document.service.ts
+++ src/document/document.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+
+import { DocumentError } from './errors';
+import {
+  DocumentTransformer,
+  DocumentTransformerOptions,
+} from './transformer/interfaces';
+import { DocumentContentType } from './types';
 
 @Injectable()
-export class DocumentService {}
+export class DocumentService {
+  private readonly logger = new Logger(DocumentService.name);
+  private readonly transformers: Record<
+    DocumentContentType,
+    DocumentTransformer
+  >;
+
+  constructor() {
+    // TODO: Implement transformers
+    this.transformers = {} as never;
+  }
+
+  public getTransformer(contentType: DocumentContentType): DocumentTransformer {
+    const transformer = this.transformers[contentType];
+    if (!transformer) {
+      throw new DocumentError(`Unsupported content type: ${contentType}`);
+    }
+    return transformer;
+  }
+
+  public convert(
+    content: unknown,
+    fromContentType: DocumentContentType,
+    toContentType: DocumentContentType,
+    options: DocumentTransformerOptions,
+  ): string {
+    this.logger.log(
+      `Converting document from ${fromContentType} to ${toContentType}`,
+    );
+    const fromTransformer = this.getTransformer(fromContentType);
+    const document = fromTransformer.parse(content, options);
+
+    const toTransformer = this.getTransformer(toContentType);
+    return toTransformer.serialize(document, options);
+  }
+}
